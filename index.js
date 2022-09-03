19f3772447492177282da10d82d7028afb63bc33
DavidLim93 <lim.david93@gmail.com>
2022-09-03 11:24:20 +1000

    Update JS, HTML and CSS

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ const form = document.getElementById('pokemon-search')
 
 function fetchPokemon() {
 
+
+
 const name = document.getElementById('search').value
 
     fetch(api + name)
     .then (response => response.json())
-    .then ( data => {
+    .then ( id => {
 
-        if (data) {
-            form.innerHTML = `
-                <h3>Pokemon not found</h3>
-            `
+        if (id) {
+          
+            pokemonCard (id)
         }
         else {
-            form.innerHTML = `
-            <h2>${data.name}</h2>
+            
+            const pokeEle= document.createElement('div');
+
+            pokeEle.classList.add('pokemon');
+
+            pokeInnerHTML = `
+                <h3>Pokemon not found</h3>
             `
+            pokeEle.innerHTML = pokeInnerHTML;
+    form.appendChild(pokeEle);
         }
     }
     )
@@ -30,8 +38,47 @@ const name = document.getElementById('search').value
 
 // fetchPokemon("charmander") 
 
+function pokemonCard (pokemon) {
+
+
+    const pokeEle = document.createElement('div');
+
+    pokeEle.classList.add('pokemon');
+
+    const {id, name, sprites, types} = pokemon;
+
+    const type = pokemon.types[0].type.name;
+
+
+
+    const pokeInnerHTML = `
+    <div class="img-container">
+    <img src="${sprites.front_default}" alt="${name}" />
+  </div>
+  <div class="info">
+    <span class="number"><strong>${id}</strong></span>
+    <h3 class="name">${name}</h3>
+    <p class="type">Type: <span>${type}</span></p>
+  </div>
+
+    `
+
+
+    pokeEle.innerHTML = pokeInnerHTML;
+    form.appendChild(pokeEle);
+
+  
+
+}
+
+function reset () {
+    document.getElementById('pokemon-search').reset
+}
+
+
 form.addEventListener("submit", function (e) {
     e.preventDefault()
+
 
     const pokemon = search.value;
     if (pokemon) {
