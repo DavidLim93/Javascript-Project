c351d6fe5aae2808d025c34daee562be8d54cd5f
DavidLim93 <lim.david93@gmail.com>
2022-09-14 18:28:13 +1000

    Updated JS

--- index.js
+++ index.js
@@ -35,15 +35,15 @@ function pokemonCard (pokemon) {
 
     pokeEle.classList.add('pokemon');
 
-    const {id, name, sprites, types} = pokemon;
-
-
+    const {id, name, sprites, types, abilities} = pokemon;
 
     const type1 = pokemon.types[0].type.name;
     const type2 = pokemon.types[1] == null ? "" : pokemon.types[1].type.name
+    const ability1 = pokemon.abilities[0].ability.name
+    const ability2 = pokemon.abilities[1] == null ? "" : pokemon.abilities[1].ability.name
+    const ability3 = pokemon.abilities[2] == null ? "" : pokemon.abilities[2].ability.name
+  
 
-
-    // pokemon.types[1].type.name
 
  const pokeInnerHTML = 
   `
@@ -54,6 +54,9 @@ function pokemonCard (pokemon) {
         <span class="number"><strong>${id}</strong></span>
         <h3 class="name" style="text-transform:uppercase">${name}</h3>
         <p class="type" style="text-transform:capitalize"><strong>Type:</strong> <span>${type1} ${type2}</span></p>
+        <p class="type" style="text-transform:capitalize"><strong>Ability:</strong> <span>${ability1}</span></p>
+        <p class="type" style="text-transform:capitalize"><strong>Ability:</strong> <span>${ability2}</span></p>
+        <p class="type" style="text-transform:capitalize"><strong>Ability:</strong> <span>${ability3}</span></p>
       </div>
         `
 
@@ -63,6 +66,7 @@ function pokemonCard (pokemon) {
 
 
 }
+
 
 
 form.addEventListener("submit", function (e) {
